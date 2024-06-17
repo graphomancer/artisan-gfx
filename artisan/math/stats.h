73b32c042dd0490667465feb29b06e0e1dd7d22a
m <green.maxim@proton.me>
2024-06-17 00:30:20 -0700

    setup for opengltut

--- artisan/math/stats.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <random>
-
-namespace artisan_gfx {
-
-double RandomDouble(double min, double max);
-
-} // namespace artisan_gfx
--- artisan/math/vec2.h
+++
@@ -1,8 +0,0 @@
-#pragma once 
-#include "vec.h"
-
-namespace artisan_gfx {
-
-typedef Vec<double, 2> Vec2;
-
-} // namespace artisan_gfx
